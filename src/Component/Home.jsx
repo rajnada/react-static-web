31d2847b38be6beb38bce201570291b072047fb6
rajnada <rajnada1434@gmail.com>
2021-02-20 10:48:04 +0530

    fix responsive issue

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -2,8 +2,8 @@ import "../Assets/scss/home.scss";
 import React from "react";
 import homeBackground from "../Assets/image/home_background.png";
 import cat_1 from "../Assets/image/cat-1.png";
-import cat_2 from "../Assets/image/cat-2.png";
-import cat_3 from "../Assets/image/cat-3.png";
+import cat_2 from "../Assets/image/craftimg.jpg";
+import cat_3 from "../Assets/image/jewellery1.jpg";
 import cat_4 from "../Assets/image/cat-4.png";
 import cat_5 from "../Assets/image/cat-5.png";
 
@@ -18,18 +18,25 @@ const Home = () => {
         <h1 className="mb-5">TOP CATEGORY</h1>
 
         <div className="gallary-img">
-          <img src={cat_1} alt="category1" className="img1" />
+          <img src={cat_1} alt="category1" className="demo1" />
           <button className="general-btn btn">hand made tools</button>
-          <img src={cat_2} alt="category2" className="img2" />
+          <img src={cat_2} alt="category2" className="demo1" />
           <button className="general-btn btn2">handicraft product</button>
-          <img src={cat_3} alt="category3" className="img3" />
+          <img src={cat_3} alt="category3" className="demo1" />
           <button className="general-btn btn3">jewelry</button>
-          <img src={cat_4} alt="category4" className="img4" />
+          <img src={cat_4} alt="category4" className="demo1" />
           <button className="general-btn btn4">furniture</button>
-          <img src={cat_5} alt="category5" className="img5" />
+          <img src={cat_5} alt="category5" className="demo1" />
           <button className="general-btn btn5">Bags & Wallets</button>
         </div>
       </div>
+      {/* <div className="btn-group"> */}
+      {/* <button className="general-btn btnn">hand made tools</button> */}
+      {/* <button className="general-btn btn2">handicraft product</button>
+          <button className="general-btn btn3">jewelry</button>
+          <button className="general-btn btn4">furniture</button>
+          <button className="general-btn btn5">Bags & Wallets</button> */}
+      {/* </div> */}
     </>
   );
 };
